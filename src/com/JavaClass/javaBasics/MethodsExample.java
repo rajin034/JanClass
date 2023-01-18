3ac333907d5817a9b6b891362be41c3554355c9e
rajpa <rjn.034@gmail.com>
2023-01-18 12:42:41 -0600

    nn

--- src/com/JavaClass/javaBasics/MethodsExample.java
+++ src/com/JavaClass/javaBasics/MethodsExample.java
@@ -1,4 +1,26 @@
 package com.JavaClass.javaBasics;
 
 public class MethodsExample {
+    int n ;
+    int m ;
+
+    public int add(){
+        return n+m;
+    }
+    public int substract(){
+        return n-m;
+    }
+    public int multiply(){
+        return n*m;
+    }
+    public double divide(){
+        return n/m;
+    }
+
+    public static void main(String[] args) {
+        MethodsExample myobj = new MethodsExample();
+        myobj.n = 10;
+        myobj.m =5;
+        System.out.println(myobj.add());
+    }
 }
