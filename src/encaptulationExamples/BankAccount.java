a3c711d93f9d3f6079ec96c867b217d9d903502b
rajin034 <rjn.034@gmail.com>
2023-01-24 15:19:50 -0600

    commit

--- src/encaptulationExamples/BankAccount.java
+++ src/encaptulationExamples/BankAccount.java
@@ -1,5 +1,13 @@
 package encaptulationExamples;
 
+
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import java.io.File;
+import java.io.IOException;
+
 public class BankAccount {
     // private fields
     private int accountNumber;
@@ -35,9 +43,22 @@ public class BankAccount {
             System.out.println("Insufficient balance");
         }
     }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "accountNumber=" + accountNumber +
+                ", balance=" + balance +
+                ", accountHolder='" + accountHolder + '\'' +
+                '}';
+    }
 }
 class main{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
+        ObjectMapper mapper = new ObjectMapper();
+        BankAccount account = mapper.readValue(new File("/Users/rajinpanthee/Desktop/JanClass/src/encaptulationExamples/account.json"), BankAccount.class);
+        System.out.println(account.toString());
+        System.out.println(account.getAccountHolder());
 
     }
 }
