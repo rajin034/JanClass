964602a01e9fb948c30941cce4cc80def536073b
rajin034 <43627925+rajin034@users.noreply.github.com>
2023-02-06 16:24:37 -0600

    set and map + problem assignment

---
+++ src/codingProblems/OccuranceOfCharacter.java
@@ -0,0 +1,39 @@
+package codingProblems;
+
+import java.util.HashMap;
+
+public class OccuranceOfCharacter
+{
+    public void countOccurance(String inputString)
+    {
+        // HashMap char as a key and occurrence as a value
+        HashMap<Character, Integer> charCountMap = new HashMap<Character, Integer>();
+
+        // Convert String to Char Array
+        char[] charArray = inputString.toCharArray();
+
+        for(char ch : charArray)
+        {
+            // If char is already present in Map increment count by one
+            if(charCountMap.containsKey(ch))
+            {
+                charCountMap.put(ch, charCountMap.get(ch)+1);
+            }
+           
+            // If char is not present set key value to 1
+            else
+            {
+                charCountMap.put(ch, 1);
+            }
+        }
+		
+        // Print CharCount Map
+        System.out.println(charCountMap.entrySet());
+    }
+
+    public static void main(String args[])
+    {
+        OccuranceOfCharacter ooc = new OccuranceOfCharacter();
+        ooc.countOccurance("abbcccdddd");
+    }
+}
--- src/codingProblems/Problem1.java
+++ src/codingProblems/Problem1.java
@@ -3,6 +3,8 @@ package codingProblems;
 import java.util.Arrays;
 
 public class Problem1 {
+
+    int a;
     public int[] twoSum(int[] nums, int target) {
         int [] arr=new int[2];
         for(int i=0;i<(nums.length-1);i++)
@@ -26,8 +28,8 @@ public class Problem1 {
 
     public static void main(String[] args) {
         Problem1 p = new Problem1();
-        int[] input = new int[]{1,3,2,4,5,6,7,8};
-        int target = 15;
+        int[] input = new int[]{1,3,2};
+        int target = 4;
         System.out.println(Arrays.toString(p.twoSum(input, target)));
     }
 }
